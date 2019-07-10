b1d98b23ae5ed3725582e299c7696b0ba7ff60ec
kennyosc <kennyoscar95@gmail.com>
2019-07-10 10:53:55 +0700

    add models type and content, install validator

--- src/index.js
+++ src/index.js
@@ -1,31 +1,59 @@
-//API
+//====API====
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const User = require('./models/user.js')
 
 const port = 2019
 const URL = 'mongodb://127.0.0.1:27017/jc9_mongoose'
 
 app.use(express.json())
 
-// MONGOOSE
+// ====MONGOOSE====
 mongoose.connect(URL, {
     // parser string URL
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-    name: String,
-    age: Number
+// ====ROUTES====
+app.get('/', (req,res)=>{
+    res.send('<h1>Website connected</h1>')
 })
 
-var person = new User({name:'Alvin', age:24})
+app.get('/users', (req,res)=>{
+    const {name,email,password,age} = req.query
 
-person.save().then(()=>{
-    console.log('Data berhasil diinput')
+    const nameData = name
+    const emailData = email
+    const passwordData = password
+    const ageData = age
+
+
+
 })
 
+app.post('/users/input', (req,res)=>{
+    const {name,email,password,age} = req.body
+
+    const nameData = name
+    const emailData = email
+    const passwordData = password
+    const ageData = age
+
+    const person = new User({
+        name: nameData,
+        email: emailData,
+        password: passwordData,
+        age: ageData
+    })
+
+    person.save().then(()=>{
+        console.log('User inserted')
+    })
+})
+
+//====PORT LISTEN====
 app.listen(port, ()=>{
     console.log('Connected to port ' + port)
 })
--- src/models/user.js
+++ src/models/user.js
@@ -1,2 +1,21 @@
 const mongoose = require('mongoose')
 
+const userSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true, // wajib diisi
+        trim: true // menghapus whitespace di awal dan akhir data
+    },
+    email: {
+        type:String,
+        validate(value){ // value: data yang diinput user
+            
+        }
+    },
+    password: String,
+    age: Number
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User;
